fc9ad4b0fb4e27845e413bf4881733e466c85297
PortalPC <ian.portal@usil.pe>
2025-05-30 01:58:10 -0500

    Actualizacion_Portal 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
 
-// put function declarations here:
-int myFunction(int, int);
+unsigned char dato_recivir = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+void config_USART (void){
+
+  UCSR0C |= (1 << UCSR0C)|( 1 << UCSZ00);
+  UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); 
+  UBRR0 = 103;
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void send_char(char c){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while(!(UCSR0A &(1 << UDR30)));
+
+  udr0 = c;
 }
+ISR(USART_RX_vect) {
+    dato_recivir = UDR0 - '0';
+    switch (dato_recivir) {
+        case 3:
+            PORTB |= 0x01;
+            break;
+        case 4:
+            PORTB &= ~(0x03);
+            break;
+            }
+}
+int main (void){
+
+  
+}
+          
